f765d14474dc762a4ea25eaf9827d8fa901f0b42
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-15 17:16:13 +0330

    prog 2: cast string to int with try catch

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -8,10 +8,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("print enter your age: ");
-        String age =  scanner.nextLine();
-        int ageInt = Integer.parseInt(age);
-        System.out.println(ageInt);
+        String age = scanner.nextLine();
+        try {
+            int ageInt = Integer.parseInt(age);
+            System.out.println(ageInt);
 //        String.valueOf(ageInt);
+        } catch (NumberFormatException ex) {
+            System.out.println("Your entered age is invalid");
+        }
+
 
     }
 }
