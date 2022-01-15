eec5ac50dd1a714e69b7a06135113d53bd3b0d56
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-15 16:46:47 +0330

    create 1: print my fullname

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -1,8 +1,20 @@
 package com.grf;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+
+        // -------------- get user firstname and lastname --------------
+        System.out.print("Enter Your first name:");
+        String firstName = scanner.nextLine();
+        System.out.print("Enter Your last name:");
+        String lastName = scanner.nextLine();
+
+        // -------------- print result ----------------
+        System.out.println("Your full name is: " + firstName + " " + lastName);
+
     }
 }
