2fb64de6d046833924d8c83c733c08f3d419baaa
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-15 17:26:22 +0330

    prog 2: cast string to int with try catch and do while

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -7,15 +7,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("print enter your age: ");
-        String age = scanner.nextLine();
-        try {
-            int ageInt = Integer.parseInt(age);
-            System.out.println(ageInt);
-//        String.valueOf(ageInt);
-        } catch (NumberFormatException ex) {
-            System.out.println("Your entered age is invalid");
-        }
+
+        do {
+            try {
+                System.out.print("print enter your age: ");
+                String age = scanner.nextLine();
+                int ageInt = Integer.parseInt(age);
+                System.out.print("Your age is: ");
+                System.out.println(ageInt);
+                // String.valueOf(ageInt);
+                break;
+            } catch (NumberFormatException ex) {
+                System.out.println("Your entered age is invalid");
+            }
+        } while (true);
 
 
     }
