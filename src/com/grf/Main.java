b292e8aefda3be14b280978c4f74feb6ff3fdfd1
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-15 17:12:17 +0330

    prog 2: cast string to int

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -7,23 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // -------------- get user firstname and lastname --------------
-        System.out.print("Enter Your first name: ");
-        String firstName = scanner.nextLine();
-        System.out.print("Enter Your last name: ");
-        String lastName = scanner.nextLine();
-
-        System.out.print("Enter Your national code: ");
-        String code = scanner.nextLine();
-
-        System.out.print("Enter Your age: ");
-        int age = scanner.nextInt();
-
-
-        // -------------- print result ----------------
-        System.out.println("full name: " + firstName + " " + lastName);
-        System.out.println("national code: " + code);
-        System.out.println("age: " + age);
+        System.out.print("print enter your age: ");
+        String age =  scanner.nextLine();
+        int ageInt = Integer.parseInt(age);
+        System.out.println(ageInt);
+//        String.valueOf(ageInt);
 
     }
 }
