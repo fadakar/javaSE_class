31e29009afb1487b1d5a86bbeab97eae2ce9badf
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-15 17:32:11 +0330

    prog 3: plus 2 user age

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -7,15 +7,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
         do {
             try {
+                System.out.print("Enter Your first name: ");
+                String firstName = scanner.nextLine();
+                System.out.print("Enter Your last name: ");
+                String lastName = scanner.nextLine();
+
+                System.out.print("Enter Your national code: ");
+                String code = scanner.nextLine();
+
+                System.out.print("Enter Your birthday: ");
+                String birthday = scanner.nextLine();
+
                 System.out.print("print enter your age: ");
                 String age = scanner.nextLine();
                 int ageInt = Integer.parseInt(age);
+                ageInt += 2;
+
+                // -------------- print result ----------------
+                System.out.println("-------------------------------------");
+                System.out.println("full name: " + firstName + " " + lastName);
+                System.out.println("national code: " + code);
+                System.out.println("birthday: " + birthday);
                 System.out.print("Your age is: ");
                 System.out.println(ageInt);
-                // String.valueOf(ageInt);
                 break;
             } catch (NumberFormatException ex) {
                 System.out.println("Your entered age is invalid");
