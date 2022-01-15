1d7c4974da9f68ed82d934691697943820a7647d
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-15 16:50:53 +0330

    prog 2: print my full name , national code , age

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -8,13 +8,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // -------------- get user firstname and lastname --------------
-        System.out.print("Enter Your first name:");
+        System.out.print("Enter Your first name: ");
         String firstName = scanner.nextLine();
-        System.out.print("Enter Your last name:");
+        System.out.print("Enter Your last name: ");
         String lastName = scanner.nextLine();
 
+        System.out.print("Enter Your national code: ");
+        String code = scanner.nextLine();
+
+        System.out.print("Enter Your age: ");
+        int age = scanner.nextInt();
+
+
         // -------------- print result ----------------
-        System.out.println("Your full name is: " + firstName + " " + lastName);
+        System.out.println("full name: " + firstName + " " + lastName);
+        System.out.println("national code: " + code);
+        System.out.println("age: " + age);
 
     }
 }
