a33c4018694e005bed0ebdd9f6a1f0e3325884dd
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-17 17:01:07 +0330

    program 6: game guess number

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -1,21 +1,36 @@
 package com.grf;
 
-import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(12);
-        numbers.add(22);
-        numbers.add(32);
+        Random rnd = new Random();
+        int selectNumber = rnd.nextInt(100);
+        System.out.println("We are selected number between 0 to 100, guess it");
+        Scanner scanner = new Scanner(System.in);
+        int counter = 1;
 
-        for (int i :numbers)
-        {
-            System.out.println(i);
-        }
+        do {
+            System.out.print("Enter Your guess number:");
+            int clientNumber = scanner.nextInt();
+            if (clientNumber == selectNumber) {
+                break;
+            } else if (selectNumber > clientNumber) {
+                System.out.println(
+                        String.format("my number is bigger than %1$d", clientNumber)
+                );
+            } else if (selectNumber < clientNumber) {
+                System.out.println(
+                        String.format("my number is smaller than %1$d", clientNumber)
+                );
+            }
+            counter++;
+        } while (true);
 
-
+        System.out.println(
+                String.format("You found number %1$d by %2$d guess", selectNumber, counter)
+        );
     }
 }
