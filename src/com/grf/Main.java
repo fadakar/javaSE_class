13d906874f18a06c7c717e09ce0cc0d68e726ce4
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-17 16:44:51 +0330

    program 5: for loop

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -1,36 +1,21 @@
 package com.grf;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(12);
+        numbers.add(22);
+        numbers.add(32);
 
-
-        System.out.print("Enter you age:");
-        int age = scanner.nextInt();
-        if (age > 0 && age < 3) {
-            System.out.println("nozad");
-        } else if (age >= 3 && age < 7) {
-            System.out.println("khordsal");
-        } else if (age >= 7 && age < 14) {
-            System.out.println("no javan");
-        } else if (age >= 14 && age < 25) {
-            System.out.println("javan");
-        } else if (age >= 25 && age < 40) {
-            System.out.println("miyan sal");
-        } else if (age >= 40 && age < 60) {
-            System.out.println("kohan sal");
-        } else if (age >= 60 && age < 80) {
-            System.out.println("salmand");
-        } else if (age >= 80 && age < 100) {
-            System.out.println("khoda biyamorz");
-        } else if (age >= 100) {
-            System.out.println("balaye 100");
-        } else {
-            System.out.println("person age not found");
+        for (int i :numbers)
+        {
+            System.out.println(i);
         }
+
 
     }
 }
