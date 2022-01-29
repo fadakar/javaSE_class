6c2d26168e8db76608d9551e16cd75c838920d7d
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-01-29 16:59:19 +0330

    program 5_1 class

--- src/com/grf/Main.java
+++ src/com/grf/Main.java
@@ -1,11 +1,30 @@
 package com.grf;
 
+import com.grf.Human.Human;
 
 public class Main {
-
     public static void main(String[] args) {
 
-        ConsoleManager consoleManager = new ConsoleManager(System.in);
-        consoleManager.run();
+
+            Human person1 = new Human("ali", 22, 78, 170);
+            Human person2 = new Human("hassan", 22, 78, 184);
+
+            Human person3 = new Human();
+            person3.setName("zahra");
+            person3.setWeight(76);
+            person3.setHeight(165);
+            try {
+                person3.setAge(28);
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+
+
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
+        System.out.println(person3.toString());
+
+
     }
+
 }
